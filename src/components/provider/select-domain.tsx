5474ae10f060e4e140af0956207001cf54a146f3
Munkhsuld <munkhsvld786@gmail.com>
2024-12-31 10:22:24 +0800

    added navbar

--- src/components/provider/select-domain.tsx
+++ src/components/provider/select-domain.tsx
@@ -14,31 +14,35 @@ import { SiBinance, SiCardano } from "react-icons/si";
 
 export default function SelectDomain() {
   const router = useRouter();
+
+  const handleValueChange = (value: string) => {
+    const selectedDomain = domains
+      .flatMap((category) => category.items)
+      .find((item) => item.name === value);
+    if (selectedDomain?.link) {
+      router.push(selectedDomain.link);
+    }
+  };
+
   return (
     <div>
-      <Select
-        defaultValue="Cardano"
-        onValueChange={(value) =>
-          domains.find((domain) => domain.name === value)?.link &&
-          router.push(
-            domains.find((domain) => domain.name === value)?.link ?? ""
-          )
-        }
-      >
+      <Select defaultValue="Cardano" onValueChange={handleValueChange}>
         <SelectTrigger className="w-[150px] focus-visible:ring-0 flex">
           <SelectValue placeholder="Select a domain" />
         </SelectTrigger>
         <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Domains</SelectLabel>
-            {domains.map((domain) => (
-              <SelectItem key={domain.id} value={domain.name}>
-                <div className="flex items-center gap-1.5">
-                  {domain.icon} {domain.name}
-                </div>
-              </SelectItem>
-            ))}
-          </SelectGroup>
+          {domains.map((domain) => (
+            <SelectGroup key={domain.category}>
+              <SelectLabel>{domain.category}</SelectLabel>
+              {domain.items.map((item) => (
+                <SelectItem key={item.id} value={item.name}>
+                  <div className="flex items-center gap-1.5">
+                    {item.icon} {item.name}
+                  </div>
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          ))}
         </SelectContent>
       </Select>
     </div>
@@ -47,27 +51,37 @@ export default function SelectDomain() {
 
 const domains = [
   {
-    id: 1,
-    name: "Ethereum",
-    link: "https://ethereum.eigenfi.io",
-    icon: <FaEthereum />,
+    category: "EVM",
+    items: [
+      {
+        id: 1,
+        name: "Ethereum",
+        link: "https://ethereum.eigenfi.io",
+        icon: <FaEthereum />,
+      },
+      {
+        id: 3,
+        name: "BNB Chain",
+        link: "https://bnb.eigenfi.io",
+        icon: <SiBinance className="w-4 h-4" />,
+      },
+      {
+        id: 4,
+        name: "BitLayer",
+        link: "https://bitlayer.eigenfi.io/",
+        icon: <FaBtc className="w-4 h-4" />,
+      },
+    ],
   },
   {
-    id: 2,
-    name: "Cardano",
-    link: "https://cardano.eigenfi.io",
-    icon: <SiCardano className="w-4 h-4" />,
-  },
-  {
-    id: 3,
-    name: "BNB Chain",
-    link: "https://bnb.eigenfi.io",
-    icon: <SiBinance className="w-4 h-4" />,
-  },
-  {
-    id: 4,
-    name: "BitLayer",
-    link: "https://bitlayer.eigenfi.io/",
-    icon: <FaBtc className="w-4 h-4" />,
+    category: "Non EVM",
+    items: [
+      {
+        id: 2,
+        name: "Cardano",
+        link: "https://cardano.eigenfi.io",
+        icon: <SiCardano className="w-4 h-4" />,
+      },
+    ],
   },
 ];
